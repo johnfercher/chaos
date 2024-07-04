ffcf9a4bd88261f436355147ccce5dd2f28f285f
John Fercher <johnathanfercher22@gmail.com>
2024-07-04 13:25:28 -0300

    add sub module

--- cmd/analysis/main.go
+++ cmd/analysis/main.go
@@ -10,7 +10,7 @@ func main() {
 	file := structservices.NewFile()
 	classifier := structservices.NewFileClassifier()
 	discover := structservices.NewDiscover(file, classifier)
-	ok, err := discover.Project("docs/medium")
+	ok, err := discover.Project("docs/examples/medium")
 	if err != nil {
 		log.Fatal(err)
 	}
